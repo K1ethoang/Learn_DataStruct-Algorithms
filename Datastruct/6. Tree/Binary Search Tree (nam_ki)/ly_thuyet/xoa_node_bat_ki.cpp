0b5988169643ec5b3b450936f8e2eb5c5c2f42e0
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-03-30 23:35:57 +0700

    update

--- Datastruct/6. Tree/Binary Search Tree (nam_ki)/ly_thuyet/xoa_node_bat_ki.cpp
+++ Datastruct/6. Tree/Binary Search Tree (nam_ki)/ly_thuyet/xoa_node_bat_ki.cpp
@@ -19,6 +19,23 @@ void menu(Node *&root);
   - 2. Node 1 con: trước khi xoá node x - chúng ta phải cập nhật mối liên kết giữa node cha của node x với node con (con trái hoặc con phải) của node x
   - 3. Node 2 con:
  */
+
+// hàm tìm node thế mạng
+void FindNodeTemp(Node *&x, Node *&y) // Node *y = root->right
+{
+    // duyệt sang bên trái nhất
+    if (y->left != NULL)
+    {
+        FindNodeTemp(x, y->left); // tìm ra node trái nhất
+    }
+    else // tìm ra được node trái nhất rồi (y->left == NULL)
+    {
+        x->data = y->data; // cập nhật cái data của node cần xoá chính là data của node thế mạng
+        x = y;             // cho node x (là node mà chúng ta sẽ xoá đi sau này) cho node x trỏ đến node thế mạng ==> ra khỏi hàm thì sẽ xoá node x
+        y = y->right;      // bản chất chỗ này chính là cập nhật lại mlk cho node cha của node thế mạng (mà chúng ta xoá) với node con của node thế mạng
+    }
+}
+
 // hàm xoá node bất kì
 void removeNode(Node *&root, const int &data)
 {
@@ -33,9 +50,9 @@ void removeNode(Node *&root, const int &data)
             removeNode(root->right, data); // duyệt sang nhánh phải của cây
         else                               // data == root->data - đã tìm ra cái node cần xoá
         {
-            Node *temp = root; // node t là node thế mạng - tí nữa ta xoá nó
-                               // xoá được cho cả node lá
-                               // nếu nhánh trái NULL <=> đây là node có 1 con chính là con phải
+            Node *x = root; // node t là node thế mạng - tí nữa ta xoá nó
+                            // xoá được cho cả node lá
+                            // nếu nhánh trái NULL <=> đây là node có 1 con chính là con phải
             if (root->left == NULL)
             {
                 root = root->right; // duyệt sang phải của cái node cần xoá
@@ -47,7 +64,15 @@ void removeNode(Node *&root, const int &data)
                 root = root->left; // duyệt sang trái của cái node cần xoá
                 // để update mlk giữa node cha của node cần xoá với node con của node cần xoá
             }
-            delete temp; // xoá node cần xoá
+            // node cần xoá là node có 2 con
+            else
+            {
+                // cách 1: Tìm node trái nhất của cây con phải (cây con phải của node cần xoá)
+                Node *y = root->right; // node y là node thế mạng cho node cần xoá - node này sẽ đảm bảo nhận nhiệm vụ là tìm ra node trái nhất
+                FindNodeTemp(x, y);
+                // cách 2: ngược lại với cách 1
+            }
+            delete x; // xoá node cần xoá
         }
     }
 }
